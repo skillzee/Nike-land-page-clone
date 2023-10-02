1259a543800919ad6c82256abc2cfdede526a646
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2023-10-02 12:55:05 +0530

    Deployment

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import PopularProduct from "./PopularProduct";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+import SpecialOffers from "./SpecialOffers";
+
+
+export{
+    Hero,
+    PopularProduct,
+    SuperQuality,
+    Services,
+    SpecialOffers,
+    CustomerReviews,
+    Subscribe,
+    Footer
+}
